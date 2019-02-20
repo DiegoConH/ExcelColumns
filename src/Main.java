0d49f10e20e3ac676e8985c435b1a2a3b12c132d
SpencerPerez <spereza2@miumg.edu.gt>
2019-02-20 11:26:03 -0600

    The columns will now be written to a file.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,41 @@
-import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Excel Columns");
-        ArrayList<String> data = new ArrayList<>();
-        // (n) Numero de columnas.
-        int c = 26;
-        for (int i = 65; i < 65 + c; i++) {
-            char temp = (char) i;
-            String item = String.valueOf(temp);
-            data.add(item);
+        StringBuilder toWrite = new StringBuilder();
+        System.out.print("Number of columns: ");
+        Scanner scanner = new Scanner(System.in);
+        int columns = scanner.nextInt();
+        columns++;
+        for (int i = 1; i < columns; i++) {
+            int j = i;
+            StringBuilder temp = new StringBuilder();
+            while (j > 0)
+            {
+                int k = (j - 1) % 26;
+                int l = 65 + k;
+                temp.insert(0, Character.toString((char) l));
+                j = (j - k) / 26;
+            }
+            toWrite.append(temp.toString());
+            if(i != columns - 1){
+                toWrite.append(";");
+            }
+        }
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("columns.csv"));
+            writer.write("sep=;\r\n");
+            writer.write(toWrite.toString());
+            writer.write("\r\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
